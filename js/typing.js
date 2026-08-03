065aa0c4311ee24dfa7fc61b4397d3c8cdb21380
ajengpa <ajengpuspaandini04@gmail.com>
2026-08-03 17:05:18 +0700

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,11 @@
+new Typed("#typing", {
+    strings: [
+        "Android Developer",
+        "Web Developer",
+        "Digital Forensics Enthusiast"
+    ],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 1800,
+    loop: true
+});
